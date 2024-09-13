42e1ac4e9180a9b4c9dbeb107b2cca26ad8848e4
Bismay Kumar <44846718+Codsssworth@users.noreply.github.com>
2024-09-13 15:50:31 +0530

    Add files via upload

---
+++ src/chatapp/src/services/keys.ts
@@ -0,0 +1,11 @@
+export const keys = {
+    DATABASE_URL: "#",
+    redis_host: "#",
+    redis_port: 17926,
+    redis_username: "default",
+    redis_password: "#",
+    username: "#",
+    password: "#",
+    brokers: "#"
+  };
+  
